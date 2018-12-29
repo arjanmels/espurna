bf64d323c533bef5073010fbddd102e3b38bc1d4
Arjan Mels <arjan.mels@gmx.net>
2018-12-29 12:13:14 +0100

    Corrected calculated frequency of LIGHT_MAX_PWM

--- code/espurna/config/general.h
+++ code/espurna/config/general.h
@@ -893,7 +893,7 @@
 #endif
 
 #if LIGHT_PROVIDER == LIGHT_PROVIDER_DIMMER
-#define LIGHT_MAX_PWM           10000        // 10000 * 200ns => 2 kHz
+#define LIGHT_MAX_PWM           10000        // 10000 * 200ns => 500Hz
 #endif
 
 #endif // LIGHT_MAX_PWM
